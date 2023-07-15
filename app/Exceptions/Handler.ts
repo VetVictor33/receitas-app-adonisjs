f3d9115e3d08935003a4fc163168017cd209b0a8
Victor Feliciano <victorjfeliciano@gmail.com>
2023-07-14 21:23:47 -0300

    prevent leak of sensible info

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -40,8 +40,7 @@ export default class ExceptionHandler extends HttpExceptionHandler {
         return ctx.response.status(400).send({message: 'Credienciais já cadastradas'})
 
       default:
-        // return ctx.response.status(500).send({message: 'Internal server error'})
-        return super.handle(error, ctx)
+        return ctx.response.status(500).send({message: 'Internal server error'})
     }
   }
 }
