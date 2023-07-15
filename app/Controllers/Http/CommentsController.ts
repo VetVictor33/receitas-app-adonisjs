436325e06c3938fea67f03401e790e8ee1bd8e1e
Victor Feliciano <victorjfeliciano@gmail.com>
2023-07-15 18:53:00 -0300

    fixed comment creation endpoint return

--- app/Controllers/Http/CommentsController.ts
+++ app/Controllers/Http/CommentsController.ts
@@ -3,20 +3,20 @@ import CommentsSchema from 'App/Schemas/CommentSchema'
 import CommentsHelper from 'App/helpers/CommentsHelper'
 
 export default class CommentsController {
-  public async store ({request, auth} : HttpContextContract) {
-    const {id: userId} = auth.user!
-    const {recipeId} = request.params()
+  public async store({ request, auth }: HttpContextContract) {
+    const { id: userId } = auth.user!
+    const { recipeId } = request.params()
 
     const validatedComment = await CommentsSchema.validate(request)
 
     const comment = await CommentsHelper.creatComment(recipeId, userId, validatedComment.content)
 
-    return {comment}
+    return comment
   }
 
-  public async destroy ({request, auth, response} : HttpContextContract) {
-    const {id: userId} = auth.user!
-    const {recipeId, id: commentId} = request.params()
+  public async destroy({ request, auth, response }: HttpContextContract) {
+    const { id: userId } = auth.user!
+    const { recipeId, id: commentId } = request.params()
 
     await CommentsHelper.deleteComment(recipeId, userId, commentId)
 
--- app/helpers/CommentsHelper.ts
+++ app/helpers/CommentsHelper.ts
@@ -5,41 +5,41 @@ import { IformatedComment } from 'App/intercafes/Comment'
 import UserHelper from './UserHelper'
 import RecipeHelper from './RecipeHelper'
 
-export default abstract class CommentsHelper{
-  public static async getRecipeAllComments (recipeId: Recipe['id']): Promise<IformatedComment[]> {
+export default abstract class CommentsHelper {
+  public static async getRecipeAllComments(recipeId: Recipe['id']): Promise<IformatedComment[]> {
     const recipe = await Recipe.findByOrFail('id', recipeId)
     const comments = await recipe.related('Comments').query().where('recipe_id', '=', recipeId)
     const formatedComments = (await this.formatMany(comments))!
     return formatedComments
   }
 
-  public static async creatComment (recipeId: Recipe['id'],
+  public static async creatComment(recipeId: Recipe['id'],
     userId: User['id'], content: string): Promise<IformatedComment> {
     await Recipe.findByOrFail('id', recipeId)
-    const comment = await Comment.create({recipeId, userId, content: content})
+    const comment = await Comment.create({ recipeId, userId, content: content })
     const formatedComment = (await this.formatOne(comment))!
     return formatedComment
   }
 
-  public static async deleteComment (
+  public static async deleteComment(
     recipeId: Recipe['id'], userId: User['id'], commentId: Comment['id']): Promise<void> {
     const recipe = await Recipe.findByOrFail('id', recipeId)
     const comment = await recipe.related('Comments').query()
       .where('recipe_id', '=', recipeId).andWhere('userId', '=', userId).andWhere('id', '=', commentId)
-    if(comment[0]){
+    if (comment[0]) {
       await comment[0].delete()
     }
   }
 
-  private static async formatOne (comment: Comment): Promise<IformatedComment> {
+  private static async formatOne(comment: Comment): Promise<IformatedComment> {
     const username = (await UserHelper.findName(comment.userId))!
     const recipeName = (await RecipeHelper.getRecipeTitle(comment.recipeId))!
-    const {id, recipeId, content, createdAt, updatedAt} = comment
-    const formatedComment = {id, username, content, recipeId, recipeName, createdAt, updatedAt}
+    const { id, recipeId, content, createdAt, updatedAt } = comment
+    const formatedComment = { id, username, content, recipeId, recipeName, createdAt, updatedAt }
     return formatedComment
   }
 
-  private static async formatMany (commentArray: Comment[]) : Promise<IformatedComment[]> {
+  private static async formatMany(commentArray: Comment[]): Promise<IformatedComment[]> {
     const allComments: IformatedComment[] = []
     for (const comment of commentArray) {
       const formatedComment = await this.formatOne(comment)
