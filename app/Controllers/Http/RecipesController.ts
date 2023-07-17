cb6d13548e6f05e7892fe50b2127e40b3efdaf36
Victor Feliciano <victorjfeliciano@gmail.com>
2023-07-17 20:17:29 -0300

    fixing minor issue

--- app/Controllers/Http/RecipesController.ts
+++ app/Controllers/Http/RecipesController.ts
@@ -3,22 +3,22 @@ import RecipeSchema from 'App/Schemas/RecipeSchema'
 import RecipeHelper from 'App/helpers/RecipeHelper'
 
 export default class RecipesController {
-  public async index ({auth}) {
-    const {id: userId} = auth.user!
-    const recipes = await RecipeHelper.findAllRecipesAndFormat({pageNumber: 1, recipePerPage: 100}, userId)
+  public async index({ auth }) {
+    const { id: userId } = auth.user!
+    const recipes = await RecipeHelper.findAllRecipesAndFormat({ pageNumber: 1, recipePerPage: 100 }, userId)
     return recipes
   }
 
-  public async paginatedIndex ({request, auth} : HttpContextContract) {
-    const {id: userId} = auth.user!
+  public async paginatedIndex({ request, auth }: HttpContextContract) {
+    const { id: userId } = auth.user!
     const validatedPagination = await RecipeSchema.validatePagination(request)
     const recipes = await RecipeHelper.findAllRecipesAndFormat(validatedPagination, userId)
     return recipes
   }
 
-  public async store ({request, auth, response} : HttpContextContract) {
+  public async store({ request, auth, response }: HttpContextContract) {
     await auth.use('api').check()
-    const {id: userId} = auth.user!
+    const { id: userId } = auth.user!
     const validatedRecipeData = await RecipeSchema.validateCreation(request)
     const file = request.file('image')!
 
@@ -28,8 +28,8 @@ export default class RecipesController {
     return newRecipe
   }
 
-  public async show ({request, auth}: HttpContextContract) {
-    const {id: userId} = auth.user!
+  public async show({ request, auth }: HttpContextContract) {
+    const { id: userId } = auth.user!
     const recipeId = +request.param('id')
 
     const recipe = await RecipeHelper.findRecipeById(recipeId, userId)
@@ -37,8 +37,8 @@ export default class RecipesController {
     return recipe
   }
 
-  public async showUsersOnly ({request, auth}: HttpContextContract){
-    const {id: userId} = auth.user!
+  public async showUsersOnly({ request, auth }: HttpContextContract) {
+    const { id: userId } = auth.user!
 
     const validatedPagination = await RecipeSchema.validatePagination(request)
     const allRecipes = await RecipeHelper.findAllUsersRecipesAndFormat(userId, validatedPagination)
@@ -46,8 +46,8 @@ export default class RecipesController {
     return allRecipes
   }
 
-  public async showUsersFavoriteRecipes ({request, auth}: HttpContextContract){
-    const {id: userId} = auth.user!
+  public async showUsersFavoriteRecipes({ request, auth }: HttpContextContract) {
+    const { id: userId } = auth.user!
 
     const validatedPagination = await RecipeSchema.validatePagination(request)
     const allRecipes = await RecipeHelper.findAllUsersFavoriteRecipesAndFormat(userId, validatedPagination)
@@ -55,8 +55,8 @@ export default class RecipesController {
     return allRecipes
   }
 
-  public async update ({request, auth, response}:HttpContextContract) {
-    const {id: userId} = auth.user!
+  public async update({ request, auth }: HttpContextContract) {
+    const { id: userId } = auth.user!
     const recipeId = +request.param('id')
     const validatedData = await RecipeSchema.validateCreation(request)
 
@@ -65,8 +65,8 @@ export default class RecipesController {
     return newRecipe
   }
 
-  public async destroy ({request, auth, response}:HttpContextContract) {
-    const {id: userId} = auth.user!
+  public async destroy({ request, auth, response }: HttpContextContract) {
+    const { id: userId } = auth.user!
     const recipeId = +request.param('id')
     await RecipeHelper.delete(recipeId, userId)
     response.status(204)
